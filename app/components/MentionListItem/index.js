261c2cabb6a96d987be99938417d68d1247a058d
Dan Hannigan <hannigan.d92@gmail.com>
2017-10-28 15:29:24 +0100

    Added PropTypes

--- app/components/MentionListItem/index.js
+++ app/components/MentionListItem/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
+import PropTypes from 'prop-types';
 
 const MentionListItem = ({ name, username }) => (
   <TouchableOpacity>
     <Text>{name}</Text>
   </TouchableOpacity>
 );
+
+MentionListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+};
 
 export default MentionListItem;
